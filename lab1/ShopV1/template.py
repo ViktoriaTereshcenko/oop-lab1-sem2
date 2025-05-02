dce51a549fa5969df8d30f4f3145bfc2d21c673e
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-02 22:28:19 +0300

    Create template.py

---
+++ lab1/ShopV1/template.py
@@ -0,0 +1,10 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), '..', 'templates')
+env = Environment(loader=FileSystemLoader(template_dir))
+
+def render_template(template_name, context=None):
+    context = context or {}
+    template = env.get_template(template_name)
+    return template.render(context)
