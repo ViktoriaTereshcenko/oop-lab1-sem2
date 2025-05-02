3bda27145d38b6c91af6481471f8589ca5388c68
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-02 22:27:52 +0300

    Create session.py

---
+++ lab1/ShopV1/session.py
@@ -0,0 +1,56 @@
+import uuid
+import secrets
+from http import cookies
+
+sessions = {}  # session_id -> {"user_id": ..., "role": ...}
+
+class SessionManager:
+
+    @staticmethod
+    def generate_session_id():
+        return secrets.token_hex(16)
+
+    @staticmethod
+    def create_session(user_id, role="client"):
+        session_id = str(uuid.uuid4())
+        sessions[session_id] = {"user_id": user_id, "role": role}
+        return session_id
+
+    @staticmethod
+    def get_session_data(cookie_header):
+        if not cookie_header:
+            return None
+        c = cookies.SimpleCookie()
+        c.load(cookie_header)
+        session_cookie = c.get("session_id")
+        if session_cookie and session_cookie.value in sessions:
+            return sessions[session_cookie.value]
+        return None
+
+    @staticmethod
+    def get_session_user_id(cookie_header):
+        session_data = SessionManager.get_session_data(cookie_header)
+        if session_data:
+            return session_data.get("user_id")
+        return None
+
+    @staticmethod
+    def clear_session(cookie_header):
+        if not cookie_header:
+            return
+        c = cookies.SimpleCookie()
+        c.load(cookie_header)
+        session_cookie = c.get("session_id")
+        if session_cookie and session_cookie.value in sessions:
+            del sessions[session_cookie.value]
+
+    @staticmethod
+    def set_user_role(user_id, role):
+        for sid, data in sessions.items():
+            if data["user_id"] == user_id:
+                data["role"] = role
+
+    @staticmethod
+    def delete_session(session_id):
+        if session_id in sessions:
+            del sessions[session_id]
