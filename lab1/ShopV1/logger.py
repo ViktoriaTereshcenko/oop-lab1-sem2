ebb59523b4dd4d20ff302f6bb87201d0595b5dc1
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-03 18:29:31 +0300

    Update logger.py

--- lab1/ShopV1/logger.py
+++ lab1/ShopV1/logger.py
@@ -1,10 +1,17 @@
 import logging
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE_PATH = os.path.join(BASE_DIR, "logs", "server.log")
+
+if not os.path.exists(LOG_FILE_PATH):
+    parent_dir = os.path.dirname(BASE_DIR)
+    LOG_FILE_PATH = os.path.join(parent_dir, "logs", "server.log")
 
 logging.basicConfig(
+    filename=LOG_FILE_PATH,
     level=logging.INFO,
-    filename='logs/server.log',
-    filemode='a',
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 def log_info(message):
