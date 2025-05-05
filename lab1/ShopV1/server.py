ec8d7777d769d1e273e07ac27bc68f7e623c413d
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-05 20:28:21 +0300

    Update server.py

--- lab1/ShopV1/server.py
+++ lab1/ShopV1/server.py
@@ -17,16 +17,16 @@ class CustomHandler(BaseHTTPRequestHandler):
         if handler:
             try:
                 session = SessionManager.get_session_data(self.headers.get("Cookie"))
-                arg_count = handler.__code__.co_argcount
-                if arg_count == 3:
+                argc = handler.__code__.co_argcount
+                if argc == 3:
                     handler(self, session)
-                elif arg_count == 2:
+                elif argc == 2:
                     handler(self, session)
-                elif arg_count == 1:
+                elif argc == 1:
                     handler(self)
                 else:
-                    raise TypeError("Unsupported number of arguments for handler")
-            except Exception as e:
+                    raise TypeError(f"Unsupported number of arguments ({argc}) for handler")
+            except Exception:
                 log_error(f"GET {self.path} failed:\n" + traceback.format_exc())
                 self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
                 self.end_headers()
@@ -43,16 +43,16 @@ class CustomHandler(BaseHTTPRequestHandler):
             try:
                 session = SessionManager.get_session_data(self.headers.get("Cookie"))
                 params = parse_post_data(self)
-                arg_count = handler.__code__.co_argcount
-                if arg_count == 3:
+                argc = handler.__code__.co_argcount
+                if argc == 4:
                     handler(self, session, params)
-                elif arg_count == 2:
+                elif argc == 3:
                     handler(self, session)
-                elif arg_count == 1:
+                elif argc == 2:
                     handler(self)
                 else:
-                    raise TypeError("Unsupported number of arguments for handler")
-            except Exception as e:
+                    raise TypeError(f"Unsupported number of arguments ({argc}) for handler")
+            except Exception:
                 log_error(f"POST {self.path} failed:\n" + traceback.format_exc())
                 self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
                 self.end_headers()
@@ -62,12 +62,10 @@ class CustomHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
-
 def run():
     httpd = HTTPServer(("", PORT), CustomHandler)
     log_info(f"Server running at http://localhost:{PORT}")
     httpd.serve_forever()
-
 
 if __name__ == "__main__":
     Router.initialize_routes()
