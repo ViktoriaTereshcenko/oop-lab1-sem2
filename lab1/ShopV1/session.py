eee627ef6145bb21c67d0291c89e07797bf0a97b
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-05 20:23:26 +0300

    Update session.py

--- lab1/ShopV1/session.py
+++ lab1/ShopV1/session.py
@@ -1,19 +1,13 @@
 import uuid
-import secrets
 from http import cookies
 
-sessions = {}  # session_id -> {"user_id": ..., "role": ...}
-
 class SessionManager:
-
-    @staticmethod
-    def generate_session_id():
-        return secrets.token_hex(16)
+    sessions = {}
 
     @staticmethod
     def create_session(user_id, role="client"):
         session_id = str(uuid.uuid4())
-        sessions[session_id] = {"user_id": user_id, "role": role}
+        SessionManager.sessions[session_id] = {"user_id": user_id, "role": role}
         return session_id
 
     @staticmethod
@@ -23,8 +17,8 @@ class SessionManager:
         c = cookies.SimpleCookie()
         c.load(cookie_header)
         session_cookie = c.get("session_id")
-        if session_cookie and session_cookie.value in sessions:
-            return sessions[session_cookie.value]
+        if session_cookie and session_cookie.value in SessionManager.sessions:
+            return SessionManager.sessions[session_cookie.value]
         return None
 
     @staticmethod
@@ -41,16 +35,15 @@ class SessionManager:
         c = cookies.SimpleCookie()
         c.load(cookie_header)
         session_cookie = c.get("session_id")
-        if session_cookie and session_cookie.value in sessions:
-            del sessions[session_cookie.value]
+        if session_cookie and session_cookie.value in SessionManager.sessions:
+            del SessionManager.sessions[session_cookie.value]
 
     @staticmethod
     def set_user_role(user_id, role):
-        for sid, data in sessions.items():
+        for sid, data in SessionManager.sessions.items():
             if data["user_id"] == user_id:
                 data["role"] = role
 
     @staticmethod
     def delete_session(session_id):
-        if session_id in sessions:
-            del sessions[session_id]
+        SessionManager.sessions.pop(session_id, None)
