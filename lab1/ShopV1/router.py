c6ebc6cc6eb3a771169be8ef989741bb541fa186
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-05 21:05:12 +0300

    Update router.py

--- lab1/ShopV1/router.py
+++ lab1/ShopV1/router.py
@@ -1,38 +1,64 @@
 from controllers.login_controller import LoginController
-from controllers.blacklist_controller import BlacklistController
-from controllers.user_controller import UserController
+from controllers.register_controller import RegisterController
+from controllers.index_controller import IndexController
 from controllers.product_controller import ProductController
+from controllers.user_controller import UserController
+from controllers.order_controller import OrderController
+from controllers.blacklist_controller import BlacklistController
 
 class Router:
     routes = {}
 
-    @classmethod
-    def initialize_routes(cls):
-        login_controller     = LoginController()
-        user_controller      = UserController()
+    @staticmethod
+    def initialize_routes():
+        login_controller = LoginController()
+        register_controller = RegisterController()
+        index_controller = IndexController()
+        product_controller = ProductController()
+        user_controller = UserController()
+        order_controller = OrderController()
         blacklist_controller = BlacklistController()
-        product_controller   = ProductController()
 
-        cls.routes = {
-            ('GET',    '/login'):       login_controller.login_form,
-            ('POST',   '/login'):       login_controller.login,
-            ('GET',    '/logout'):      login_controller.logout,
-            ('GET',    '/register'):    user_controller.register_form,
-            ('POST',   '/register'):    user_controller.register,
-            ('GET',    '/admin/users'): user_controller.list_users,
-            ('GET',    '/blacklist'):   blacklist_controller.list_blacklist,
-            ('POST',   '/blacklist'):   blacklist_controller.add_to_blacklist,
-            ('GET',    '/'):                      product_controller.list_products,
-            ('GET',    '/products'):              product_controller.list_products,
-            ('GET',    '/products/create'):       product_controller.create_form,
-            ('POST',   '/products/create'):       product_controller.create,
-            ('POST',   '/products/delete'):       product_controller.delete,
+        Router.routes = {
+            # Головна
+            ('GET', '/'): lambda handler, session: Router.redirect_to_login(handler),
+            ('GET', '/index'): index_controller.index,
+
+            # Аутентифікація
+            ('GET', '/login'): login_controller.login_form,
+            ('POST', '/login'): login_controller.login,
+            ('GET', '/logout'): login_controller.logout,
+
+            # Реєстрація
+            ('GET', '/register'): register_controller.register_form,
+            ('POST', '/register'): register_controller.register,
+
+            # Користувачі
+            ('GET', '/users'): user_controller.list_users,
+
+            # Продукти
+            ('GET', '/products'): product_controller.list_products,
+            ('GET', '/products/create'): product_controller.create_form,
+            ('POST', '/products/add'): product_controller.create,
+            ('POST', '/products/delete'): product_controller.delete,
+
+            # Замовлення
+            ('GET', '/orders'): order_controller.list_orders,
+            ('GET', '/orders/create'): order_controller.create_form,
+            ('POST', '/orders/add'): order_controller.create,
+            ('POST', '/orders/pay'): order_controller.pay,
+
+            # Чорний список
+            ('GET', '/blacklist'): blacklist_controller.list_blacklist,
+            ('POST', '/blacklist'): blacklist_controller.add_to_blacklist,
+            ('POST', '/blacklist/remove'): blacklist_controller.remove_from_blacklist,
         }
 
-    @classmethod
-    def add_route(cls, path, method, handler):
-        cls.routes[(method, path)] = handler
+    @staticmethod
+    def get_handler(path, method):
+        return Router.routes.get((method, path))
 
-    @classmethod
-    def get_handler(cls, path, method):
-        return cls.routes.get((method, path))
+    @staticmethod
+    def redirect_to_login(handler):
+        from utils import redirect
+        redirect(handler, '/login')
