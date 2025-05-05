b302dac772ed40cf3ed590a5431ce6085adf162e
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-05-05 15:09:48 +0300

    Update database.py

--- lab1/ShopV1/database.py
+++ lab1/ShopV1/database.py
@@ -1,10 +1,9 @@
-import os
 import psycopg
 
 def get_connection():
     return psycopg.connect(
         host="localhost",
-        dbname=os.environ.get("DB_NAME", "your_db_name"),
-        user=os.environ.get("DB_USER", "your_db_user"),
-        password=os.environ.get("DB_PASSWORD", "your_db_password")
+        dbname="shop",
+        user="vikilinater",
+        password="admin"
     )
